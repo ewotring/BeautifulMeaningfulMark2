04c8f2b22b986c152a0f1f998d588db432d78a71
Erik Wotring <ewotring@gmail.com>
2017-08-20 22:14:13 -0500

    WIP User routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,31 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
+  include BCrypt
+
   has_many :poems
   has_many :comments
   # Update schema to reflect that users create comments
+
+  validates :email, presence: true
+  validate :validate_password
+
+  def password
+  	@password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+  	@plain_text_password = new_password
+  	@password = Password.create(new_password)
+  	self.password_hash = @password
+  end
+
+  def authenticate(new_password)
+  	self.password == new_password
+  end
+
+  def validate_password
+  	if @plain_text_password.nil?
+  		errors.add(:password, "cannot be blank")
+  	end
+  end
 end
