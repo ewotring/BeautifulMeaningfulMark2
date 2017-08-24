b1d15bf141aa7742604694edd5e1d623ad30b084
Erik Wotring <email": null>
2017-08-24 10:49:37 -0500

    Create seeds; got distracted working on User routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
 10.times do
-  User.create!(username: Faker::Name.first_name, password: "password", email: Faker::Internet.safe_email)
+  User.create!(username: Faker::Lovecraft.deity, password: "password", email: Faker::Internet.safe_email)
+end
+
+10.times do
+  Poem.create!(name: Faker::Lovecraft.word, body: Faker::Lovecraft.paragraph, user_id: rand(1..10))
+end
+50.times do
+  Comment.create!(user_id: rand(1..10), poem_id: rand(1..10), name: Faker::Lovecraft.word, body: Faker::Lovecraft.sentence)
 end
